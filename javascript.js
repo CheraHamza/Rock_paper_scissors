f2ff1e0433737a8b9964d8596d68b4edd97ced31
Hamza Chera w <cherahamza1@gmail.com>
2022-06-19 23:27:50 +0100

    add playerselection function. add comments to indicate each function's job.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,25 @@
+//lets the computer make a random choice, then returns the result.
 function computerPlay()
 {
     let random=(Math.floor(Math.random()*100)+1);
     if (random<=(100/3))
     {
-        return 'Rock';
+        return 'rock';
     }
     else if (random<(200/3))
     { 
-        return 'Paper';
+        return 'paper';
     }
     else
     { 
-        return 'Scissors';
+        return 'scissors';
     }
 }
+// prompts the user to type in his choice.
+function playerSelection()
+{ 
+    let choice= (prompt("choose: rock, paper or scissors ",''));
+    return (choice.toLowerCase());
+}
+
+
